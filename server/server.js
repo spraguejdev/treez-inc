90709eb31e4ad4119e137bd93b84f748e4fecbe8
John Sprague <spraguej624@gmail.com>
2019-07-06 12:49:05 -0700

    Revert "Initialize React-Virtualized Table"

--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var PORT = 3000;
-app.use(express.static(__dirname + '/../client/dist'));
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
